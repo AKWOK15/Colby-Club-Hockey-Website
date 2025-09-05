43020ae0c224a2df39d1a79d5ecee887e6af6119
Aidan Kwok <121518425+AKWOK15@users.noreply.github.com>
2025-09-05 18:30:25 -0400

    Adjusting schedule

--- functions/getschedule.js
+++ functions/getschedule.js
@@ -4,13 +4,12 @@ const headers = {
 };
 const years = [2023, 2024];
 exports.handler = async (event) => {
-    if(event.queryStringParameters.year==2024){
+    if(event.queryStringParameters.year==2025){
+        data = require('./schedule2025.json');
+    }
+    else if(event.queryStringParameters.year==2024){
         data = require('./schedule2024.json');
     }
-    // if (years.includes(parseInt(event.queryStringParameters.year))){    
-    //     url = `./schedule${event.queryStringParameters.year}.json`;
-    //     data = require(url);
-    // }
     else if(event.queryStringParameters.year==2023){
         data = require('./schedule2023.json');
     }
